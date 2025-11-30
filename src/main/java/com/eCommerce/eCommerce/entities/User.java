b24a92bf58c12b217ff607005d8af959a3a5ed07
abdelruhmanfared <abdelr7manfared@gmail.com>
2025-12-01 00:31:24 +0200

    feat(User): add user entity

--- src/main/java/com/eCommerce/eCommerce/entities/User.java
+++ src/main/java/com/eCommerce/eCommerce/entities/User.java
@@ -1,4 +1,35 @@
 package com.eCommerce.eCommerce.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name = "id")
+    private Long id;
+
+    @JoinColumn(name = "name")
+    private String name;
+
+
+    @JoinColumn(name = "email")
+    private String email;
+
+    @JoinColumn(name = "password")
+    private String password;
+
+
+
+
+
 }
