4789fcd80d1c5f08e661b85e2c13496474ffaf07
abdelruhmanfared <abdelr7manfared@gmail.com>
2025-12-05 03:21:51 +0200

    feat(Auth) : create auth

--- src/main/java/com/eCommerce/eCommerce/controllers/AuthController.java
+++ src/main/java/com/eCommerce/eCommerce/controllers/AuthController.java
@@ -1,24 +1,37 @@
 package com.eCommerce.eCommerce.controllers;
 
 import com.eCommerce.eCommerce.dtos.requests.LoginRequest;
+import com.eCommerce.eCommerce.dtos.responses.JwtResponse;
+import com.eCommerce.eCommerce.dtos.responses.UserDto;
 import com.eCommerce.eCommerce.services.AuthService;
+import com.eCommerce.eCommerce.services.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final UserService userService;
     @PostMapping("/login")
-        public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request){
-            authService.login(request);
-            return ResponseEntity.ok().build();
+        public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request){
+            var jwtResponse = authService.login(request);
+            return ResponseEntity.ok().body(jwtResponse);
+    }
+    @PostMapping("/validate")
+    public boolean validate(@RequestHeader("Authorization") String token) {
+        token = token.replace("Bearer ","");
+        return authService.validate(token);
+    }
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var userId = (Long)authentication.getPrincipal();
+        var userDto =  userService.getUser(userId);
+        return ResponseEntity.ok(userDto);
     }
 }
--- src/main/java/com/eCommerce/eCommerce/services/AuthService.java
+++ src/main/java/com/eCommerce/eCommerce/services/AuthService.java
@@ -1,6 +1,11 @@
 package com.eCommerce.eCommerce.services;
 
 import com.eCommerce.eCommerce.dtos.requests.LoginRequest;
+import com.eCommerce.eCommerce.dtos.responses.JwtResponse;
+import com.eCommerce.eCommerce.dtos.responses.UserDto;
+import com.eCommerce.eCommerce.exceptions.EmailNotFoundException;
+import com.eCommerce.eCommerce.mappers.UserMapper;
+import com.eCommerce.eCommerce.repositories.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,11 +15,22 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Service
 public class AuthService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
     private final AuthenticationManager authenticationManager;
-    public void login(@Valid LoginRequest request) {
+    private final JwtService jwtService;
+    public JwtResponse login(@Valid LoginRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 request.getEmail(),
                 request.getPassword()
         ));
+        var user = userRepository .findByEmail(request.getEmail()).orElseThrow(EmailNotFoundException::new);
+        var token = jwtService.genrateToken(user);
+        return new JwtResponse(token);
     }
+
+    public boolean validate(String token) {
+        return jwtService.validateToken(token);
+    }
+
 }
